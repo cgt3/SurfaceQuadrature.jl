624df8ce147e8d0637c6bdf926b49880913ec91a
Christina <cgt3@rice.edu>
2022-05-09 17:55:08 -0500

    Added first quadrature function

--- src/SurfaceQuadrature.jl
+++ src/SurfaceQuadrature.jl
@@ -1,23 +1,39 @@
 module SurfaceQuadrature
 
 using ForwardDiff
+using LinearAlgebra
 using Revise
 
 export get_line_quadrature
+export get_all_line_quadratures
 
+function get_line_quadrature(ref_quadr, curve, s_domain, curveParams...; ref_domain=(-1,1), normalization="unnormalized")
 
-function get_line_quadrature(ref_quadr, curve, s_domain, curveParams...; ref_domain=(-1,1))
+    # Map the reference quadr points to s values
+    ref_pts, ref_weights = ref_quadr
     x_lb, x_ub = ref_domain
     s_lb, s_ub = s_domain
+    s_pts = @. (s_ub - s_lb)/(x_ub - x_lb)*(ref_pts - x_lb) + s_lb
 
-    ref_pts, ref_weights = ref_quadr
-    # TODO: should return the mapped s values or the (x,y...) points associated with the curve?
-    line_pts = @. ref_pts + (s_ub - s_lb)/(x_ub - x_lb)*(ref_pts - x_lb) + s_lb
-
+    # Calculate the derivative and normal values at each of the s values
     dc_ds(s) = ForwardDiff.derivative(t->curve(t,curveParams...), s)
-    line_weights = ref_weights.*dc_ds(line_points)
+    tangents = @. dc_ds(s_pts)
 
-    return (line_pts, line_weights)
+    normals = [ @. tangents[i][2], -tangents[i][1] for i in 1:length(tangents) ]
+
+    if normalization == "normalized"
+        normals = @. normals / norm(normals)
+    end
+
+    # Calculate the evaluation points and adjusted weights
+    line_pts = @. curve(s_pts, curveParams...)
+    line_weights = @. ref_weights.*norm(dc_ds(s_pts))
+
+    return line_pts, line_weights, normals
 end
 
+function get_all_line_quadratures(ref_quadr, curve, stop_points, curveParams; ref_domain=(-1,1))
+
 end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using SurfaceQuadrature
+using GaussQuadrature
 using Test
 
 @testset "SurfaceQuadrature.jl" begin
-    # Write your tests here.
+    @testset "get_line_quadrature" begin
+        # 1. Identity mapping
+        ref_pts, ref_wts = legendre(5) # Legendre-Gauss quadrature
+        ref_pts_2D = [ [ref_pts[i], 0] for i in 1:length(ref_pts)]
+
+        line(s) = [s, 0*s] # simple linear function
+        s_domain = (-1, 1)
+
+        line_pts, line_wts, normals = get_line_quadrature((ref_pts, ref_wts), line, s_domain)
+        @test line_wts == ref_wts
+        @test length(line_pts) == length(ref_pts)
+
+        for i = 1:length(ref_pts)
+            diff = abs.(line_pts[i] - ref_pts_2D[i])
+            @test maximum(diff) < 1e-15
+        end
+
+    end # "get_line_quadrature"
 end
